b8a83476113eb119bfb173517eced2fafe8c6db1
KleberDevMen <klebersubcontas@gmail.com>
2019-04-24 15:53:33 -0300

    form_with; edit admin (não completo)

--- app/controllers/users_backoffice/welcome_controller.rb
+++ app/controllers/users_backoffice/welcome_controller.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
+
 class UsersBackoffice::WelcomeController < UsersBackofficeController
-  def index
-  end
+  def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
   end
+
   namespace :users_backoffice do
     get 'welcome/index'
   end
+
   namespace :admins_backoffice do
-    get 'welcome/index'
+    get 'welcome/index' # Dashboard
+    resources :admins, only: [:index, :edit, :update] # Admininistradores
   end
+
   devise_for :admins
   devise_for :users
   root to: 'site/welcome#index'
